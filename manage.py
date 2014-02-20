4baa892d7ed1c3092e09c7ff95f2377b6c44b4d8
Matthew McCants <mattmccants@gmail.com>
2014-02-19 20:33:02 -0600

    Gave up on running custom tests from manage.py

--- manage.py
+++ manage.py
@@ -9,35 +9,3 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
-
-
-
-
-
-
-
-
-
-
-    """
-    if sys.argv[1] == 'test':
-        from django.test.runner import DiscoverRunner
-        
-
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-            'words.tests.test_settings')
-
-        test_runner = DiscoverRunner(verbosity=1)
-        failures = test_runner.run_tests(['words',])
-        if failures:
-            sys.exit(failures)
-
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                            'words.tests.persistent_settings')
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
-"""
