bea3a0b704e0da2a3c46fecc7901655014ad5f26
Matthew McCants <mattmccants@gmail.com>
2014-02-19 20:16:07 -0600

    Removed Django 1.5.5 support for now.

--- runtests.py
+++ runtests.py
@@ -3,18 +3,18 @@ import sys
 
 import django
 from django.conf import settings
-from django.test.utils import get_runner
-
-from words.tests import test_settings
 
 
 if not settings.configured:
+	from words.tests import test_settings
 	settings.configure(**test_settings.__dict__)
 
 def runtests(*test_args):
 	if hasattr(django, 'setup'):
 		django.setup()
 
+	from django.test.utils import get_runner
+	
 	apps = sys.argv[1:] or ['words',]
 	TestRunner = get_runner(settings)
 	test_runner = TestRunner(verbosity=2, interactive=True)
