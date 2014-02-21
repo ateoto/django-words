8eb3d1730914826befd21a84230fe40d4a73c668
Matthew McCants <mattmccants@gmail.com>
2014-02-21 11:20:21 -0600

    Some pep8 stuff.

--- runtests.py
+++ runtests.py
@@ -6,20 +6,21 @@ from django.conf import settings
 
 
 if not settings.configured:
-	from words.tests import test_settings
-	settings.configure(**test_settings.__dict__)
+    from words.tests import test_settings
+    settings.configure(**test_settings.__dict__)
+
 
 def runtests(*test_args):
-	if hasattr(django, 'setup'):
-		django.setup()
+    if hasattr(django, 'setup'):
+        django.setup()
 
-	from django.test.utils import get_runner
-	
-	apps = sys.argv[1:] or ['words',]
-	TestRunner = get_runner(settings)
-	test_runner = TestRunner(verbosity=2, interactive=True)
-	failures = test_runner.run_tests(apps)
-	sys.exit(failures)
+    from django.test.utils import get_runner
+
+    apps = sys.argv[1:] or ['words', ]
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=2, interactive=True)
+    failures = test_runner.run_tests(apps)
+    sys.exit(failures)
 
 if __name__ == '__main__':
     runtests(*sys.argv[1:])
