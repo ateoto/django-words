5daf7949a956f632cce915bf80f7a7a5a6a9ea4a
Matthew McCants <mattmccants@gmail.com>
2014-02-25 18:58:06 -0600

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -53,6 +53,9 @@ setup(
     include_package_data=True,
     install_requires=[
         'django',
+        'django-crispy-forms',
+        'django-taggit',
         'django-ckeditor-updated',
+        'pytz',
     ],
 )
