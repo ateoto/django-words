dcdcd5c0b05f630fd4111728963b8c33d4054606
Matthew McCants <mattmccants@gmail.com>
2014-04-29 18:48:14 -0500

    Ignore South Migrations, fix persistent testing settings.

--- words/tests/persistent_settings.py
+++ words/tests/persistent_settings.py
@@ -1,5 +1,14 @@
 from .test_settings import *  # NOQA
 
+EXTERNAL_APPS += ['south']
+
+INSTALLED_APPS = EXTERNAL_APPS + INTERNAL_APPS
+
+SOUTH_MIGRATION_MODULES = {
+	'taggit': 'taggit.south_migrations',
+    'words': 'words.south_migrations',
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
